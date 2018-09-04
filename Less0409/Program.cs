cd6022ae33e407e5d1d2aed1ad1ec8252a1944e2
КимС <@224_10>
2018-09-04 19:17:34 +0600

    Добавьте файлы проекта.

---
+++ Less0409/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less0409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
